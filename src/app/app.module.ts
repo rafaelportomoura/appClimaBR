ac96a702281d5667baaf1a395d9056fc7cce80f2
rafael.moura1 <rafael.moura1@estudante.ufla.br>
2021-03-26 20:59:57 -0300

    Revert "fake data"
    This reverts commit 05dcae6802a57672595f2e181f77ef847a70737b.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,3 @@
-import { FakeCityRepository } from './../data/fake/fake-city-repository';
-import { SearchCityService } from './../domain/services/search-city.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -9,17 +7,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-const createSearchCityService = () => new SearchCityService(new FakeCityRepository())
-
-
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    {provide: SearchCityService, useFactory: createSearchCityService}
-  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,4 @@
-import { SearchCityService } from './../../domain/services/search-city.service';
-import { City } from './../../domain/entities/city';
 import { Component } from '@angular/core';
-
 
 @Component({
   selector: 'app-home',
@@ -9,19 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  cities: City[];
-  hasError: boolean = false;
-  errorMessage: string;
-  constructor(private readonly searchCity: SearchCityService) {}
 
-  async onSearch(query: string){
-    this.hasError = false;
-    try {
-      this.cities = await this.searchCity.search(query);
-    } catch(error){
-      this.hasError = true;
-      this.errorMessage = error.message;
-    }
-  }
+  constructor() {}
 
 }
